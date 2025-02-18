01ccb0bc73e456faffbb511c86a3a50dfd148711
kamja44 <gudgh5007@gmail.com>
2025-02-18 13:44:10 +0900

    One To One Relationship 작업해보기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,12 +3,15 @@ import { AppService } from './app.service';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Role, UserModel } from './entity/user.entity';
 import { Repository } from 'typeorm';
+import { ProfileModel } from './entity/profile.entity';
 
 @Controller()
 export class AppController {
   constructor(
     @InjectRepository(UserModel)
     private readonly userRepository: Repository<UserModel>,
+    @InjectRepository(ProfileModel)
+    private readonly profileRepository: Repository<ProfileModel>,
   ) {}
   @Post('users')
   postUser() {
@@ -19,7 +22,11 @@ export class AppController {
   }
   @Get('users')
   getUsers() {
-    return this.userRepository.find();
+    return this.userRepository.find({
+      relations: {
+        profile: true,
+      },
+    });
   }
   @Patch('users/:id')
   async patchUser(@Param('id') id: string) {
@@ -31,7 +38,21 @@ export class AppController {
 
     return this.userRepository.save({
       ...user,
-      title: user.title + '0',
+      // title: user.title + '0',
     });
+  }
+
+  @Post('user/profile')
+  async CreateUserAndProfile() {
+    const user = await this.userRepository.save({
+      email: 'kamja@kamja.co',
+    });
+
+    const profile = await this.profileRepository.save({
+      profileImg: 'test.jpg',
+      user,
+    });
+
+    return user;
   }
 }
--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -3,11 +3,13 @@ import {
   CreateDateColumn,
   Entity,
   Generated,
+  OneToOne,
   PrimaryColumn,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
   VersionColumn,
 } from 'typeorm';
+import { ProfileModel } from './profile.entity';
 
 export enum Role {
   USER = 'user',
@@ -32,33 +34,34 @@ export class UserModel {
   //   @PrimaryGeneratedColumn('uuid')
   @PrimaryGeneratedColumn()
   id: number;
-
+  @Column()
+  email: string;
   // 제목
-  @Column({
-    // 데이터베이스에서 인지하는 칼럼 타입
-    // 자동으로 유추됨 => 특정한 타입을 원할 경우 직접 입력
-    type: 'varchar',
-    // 데이터베이스 칼럼 이름
-    // 프로퍼티 이름으로 부터 자동 유추됨
-    name: 'title',
-    // 값의 길이
-    // 입력 할 수 있는 글자의 길이가 300
-    length: 300,
-    // null 허용 여부
-    nullable: true,
-    // true면 처음 저장할 때만 값 저장 가능
-    // 이후에는 값 변경 불가능
-    update: true,
-    // find()를 실행할 때 기본으로 값을 불러올지 여부
-    // 기본값이 true
-    select: false,
-    // 아무것도 입력하지 않았을 때 기본으로 입력되게 하는 값
-    default: 'default value',
-    // 칼럼중 유일무이한 값이 되어야 하는지 여부
-    // 기본값은 false
-    unique: false,
-  })
-  title: string;
+  // @Column({
+  //   // 데이터베이스에서 인지하는 칼럼 타입
+  //   // 자동으로 유추됨 => 특정한 타입을 원할 경우 직접 입력
+  //   type: 'varchar',
+  //   // 데이터베이스 칼럼 이름
+  //   // 프로퍼티 이름으로 부터 자동 유추됨
+  //   name: 'title',
+  //   // 값의 길이
+  //   // 입력 할 수 있는 글자의 길이가 300
+  //   length: 300,
+  //   // null 허용 여부
+  //   nullable: true,
+  //   // true면 처음 저장할 때만 값 저장 가능
+  //   // 이후에는 값 변경 불가능
+  //   update: true,
+  //   // find()를 실행할 때 기본으로 값을 불러올지 여부
+  //   // 기본값이 true
+  //   select: false,
+  //   // 아무것도 입력하지 않았을 때 기본으로 입력되게 하는 값
+  //   default: 'default value',
+  //   // 칼럼중 유일무이한 값이 되어야 하는지 여부
+  //   // 기본값은 false
+  //   unique: false,
+  // })
+  // title: string;
 
   @Column({
     type: 'enum',
@@ -89,4 +92,6 @@ export class UserModel {
   // Generated Annotation은 Column Annotation과 항상 같이 사용해야 한다.
   @Generated('uuid')
   additionalId: string;
+  @OneToOne(() => ProfileModel, (profile) => profile.user)
+  profile: ProfileModel;
 }
